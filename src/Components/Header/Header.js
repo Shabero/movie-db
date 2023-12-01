e85639a78b35c78a8f09ac89e9e4c3ba139c645c
Shabdan <tokmokiuca@gmail.com>
2023-12-01 12:56:36 +0600

    new

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header class={'header'} className={"p-3 bg-opacity-75  bg-dark fixed-top "}>
+
             <nav className="navbar navbar-dark ">
                 <div className="container-fluid justify-content-around">
                     <div className={'links'}>
--- src/Views/SearchPage/SearchPage.js
+++ src/Views/SearchPage/SearchPage.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from "../../Components/Header/Header";
+import axios from "axios";
+import CocktailList from "../../Components/MovieList/MovieList";
 
 const SearchPage = () => {
+    const [value, setValue] = useState([])
+    const [name, setName] = useState('')
+    const searchDrinks = () => {
+        axios(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+            .then(({data}) => setValue(data.drinks))
+    }
+    console.log(value)
+    const handleGetValue = (e) => {
+        setName(e.target.value)
+        console.log(name)
+    }
     return (
-        <div>
-            
-        </div>
-    );
-};
-
+        <>
+            <Header />
+            <div className={'container'}>
+                <h2 className={'p-4'}><b>Search</b></h2>
+                <div className={"d-flex align-items-center p-4"}>
+                    <input type="text" className={"p-2 rounded-3 border-dark border-1"} onChange={handleGetValue}/>
+                    <button className={"p-2 btn btn-outline-dark border-2"} onClick={searchDrinks}>Search</button>
+                </div>
+                <CocktailList drinks={value} />
+            </div>
+        </>
+    )
+}
 export default SearchPage;
